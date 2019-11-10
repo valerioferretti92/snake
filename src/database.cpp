2aa4f6bcc276a3ee083bb6af7fd1765bcca5159f
Valerio Ferretti <valerio.ferretti92@gmail.com>
2019-11-10 13:46:34 +0100

    Exception handling fix and error condition checking when creating a folder. Exception handling has been fixed in db connection class. Throwing a string literal as exception caused the catch clause to fail. Now, the literal is used as parameter of a string object. Added error checking code when creating db folder. An excpetion is raised when creation fails for reasons other than the fact that the folder already exists.

--- src/database.cpp
+++ src/database.cpp
@@ -11,12 +11,15 @@ Database::Database(){
   int sqlite_rcode, directory_rcode;
 
   //creating db folder in user's home directory
-  mkdir(getDbFolderPath().c_str(), S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IXGRP |  S_IROTH | S_IXOTH);
+  directory_rcode = mkdir(getDbFolderPath().c_str(), S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IXGRP |  S_IROTH | S_IXOTH);
+  if(directory_rcode == -1 && errno != EEXIST){
+    throw string("Could not create db folder!");
+  }
 
   //initializing sqlite3 library and opening db connection
   sqlite3_initialize();
   sqlite_rcode = sqlite3_open_v2(getDbFilePath().c_str(), &dbConnection, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
-  if(sqlite_rcode != SQLITE_OK) throw "Could not open db!";
+  if(sqlite_rcode != SQLITE_OK) throw string("Could not open db!");
 }
 
 Database::~Database(){
@@ -25,7 +28,7 @@ Database::~Database(){
   string forcedClosing = "Forcibly closing db!";
   string msg;
 
-  //closing down db connection (3 attempts)
+  //closing down db connection (3 attempts) and deallocating resources assigned to sqlite3
   while(attempt <= 3){
     sqlite_rcode = sqlite3_close(dbConnection);
     if(sqlite_rcode == SQLITE_OK) break;
@@ -34,8 +37,6 @@ Database::~Database(){
     this_thread::sleep_for(chrono::milliseconds(1000));
     attempt++;
   }
-
-  //deallocating resources assigned to sqlite3
   sqlite3_shutdown();
 }
 
